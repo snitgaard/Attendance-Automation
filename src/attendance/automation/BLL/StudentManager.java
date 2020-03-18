25809301cbcc909f8bfd843fabd237af9bfc33cd
FimFi <55874743+FimFi@users.noreply.github.com>
2020-03-18 10:17:33 +0100

    Connection through program

--- src/attendance/automation/BLL/StudentManager.java
+++ src/attendance/automation/BLL/StudentManager.java
@@ -6,13 +6,31 @@
 package attendance.automation.BLL;
 
 import attendance.automation.BE.Student;
+import attendance.automation.DAL.StudentDAO;
+import java.io.IOException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author jigzi
  */
 public class StudentManager {
+
+    private StudentDAO studentDAO;
+    
+    public StudentManager() {
+        try
+        {
+            studentDAO = new StudentDAO();
+        } catch (IOException ex)
+        {
+            Logger.getLogger(StudentManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
 
     public List<Student> getAllData() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
