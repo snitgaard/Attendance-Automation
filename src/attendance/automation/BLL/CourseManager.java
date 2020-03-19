cd5e40a58c34fde56b23b40822adedb06f141441
FimFi <55874743+FimFi@users.noreply.github.com>
2020-03-19 09:37:57 +0100

    Course CRUD

---
+++ src/attendance/automation/BLL/CourseManager.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package attendance.automation.BLL;
+
+import attendance.automation.BE.Course;
+import attendance.automation.DAL.CourseDAO;
+import attendance.automation.DAL.DalException;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author jigzi
+ */
+public class CourseManager {
+     private CourseDAO courseDao;
+
+    public CourseManager()
+    {
+
+        try
+        {
+            courseDao = new CourseDAO();
+        } catch (IOException ex)
+        {
+            Logger.getLogger(CourseManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    // This method calls the createCourse method in the courseDao
+    public boolean createCourse(String courseName, int courseId)
+    {
+        return courseDao.createCourse(courseName, courseId);
+    }
+
+    // This song tries to call getAllCourses method from courseDao
+    public List<Course> getAllCourses()
+    {
+        try
+        {
+            return courseDao.getAllCourses();
+        } catch (SQLException ex)
+        {
+            Logger.getLogger(CourseManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    // This method calls the method deleteCourse in the courseDao
+    public void deleteSong(Course course) throws DalException
+    {
+        courseDao.deleteCourse(course);
+    }
+
+    //This method calls the method updateCourse method from the courseDao
+    public boolean updateCourse(String courseName, int courseId)
+    {
+        return courseDao.updateCourse(courseName, courseId);
+    }
+}
---
+++ src/attendance/automation/gui/Model/CourseModel.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package attendance.automation.gui.Model;
+
+import attendance.automation.BE.Course;
+import attendance.automation.BLL.CourseManager;
+import attendance.automation.DAL.DalException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+/**
+ *
+ * @author jigzi
+ */
+public class CourseModel {
+    private ObservableList<Course> allCourses;
+    private CourseManager courseManager;
+
+    
+    public CourseModel()
+    {
+        courseManager = new CourseManager();
+    }
+
+    //This is what the controller calls when trying to show a list of all Courses. This calls a method in the CourseManager
+    public ObservableList<Course> getAllCourses()
+    {
+        allCourses = FXCollections.observableArrayList();
+        allCourses.addAll(courseManager.getAllCourses());
+        return allCourses;
+    }
+
+
+    //This is what the controller calls when creating a Course. This calls a method in the CourseManager
+    public void createCourses(String courseName, int courseId) throws DalException
+    {
+        boolean courseIsCreated = courseManager.createCourse(courseName, courseId);
+        if (courseIsCreated)
+        {
+            System.out.println("Course Created");
+        }
+    }
+
+    //This is what the controller calls when deleting a course. This calls a method in the CourseManager
+    public void deleteCourse(Course selectedCourse) throws DalException
+    {
+        courseManager.deleteSong(selectedCourse);
+        if (allCourses.remove(selectedCourse))
+        {
+            allCourses.remove(selectedCourse);
+        }
+    }
+
+    //This is what the controller calls when updating the name of a course. This calls a method in the CourseManager
+    public void updateCourse(String courseName, int courseId)
+    {
+        boolean courseIsUpdated = courseManager.updateCourse(courseName, courseId);
+        if (courseIsUpdated)
+        {
+            System.out.println("course Is Updated");
+        }
+    }
+}
