2b283fc30807b0b3a72cef2d606cfc42066eb0e4
SerireS <55481182+SerireS@users.noreply.github.com>
2020-03-19 10:15:33 +0100

    Functional: Can only register attandence on school ip

--- src/attendance/automation/gui/controller/StudentAttendanceController.java
+++ src/attendance/automation/gui/controller/StudentAttendanceController.java
@@ -10,13 +10,17 @@ import attendance.automation.gui.Model.StudentModel;
 import com.jfoenix.controls.JFXProgressBar;
 import com.jfoenix.controls.JFXToggleButton;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.URL;
+import java.net.UnknownHostException;
 import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -25,9 +29,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -37,14 +45,14 @@ import javafx.stage.StageStyle;
  *
  * @author The Best Group
  */
-public class StudentAttendanceController implements Initializable
-{
+public class StudentAttendanceController implements Initializable {
+
     StudentModel studentModel;
     Student student;
+    private String IpAddress;
 
     @FXML
     private ImageView btn_close;
-
     private double xOffset = 0;
     private double yOffset = 0;
     @FXML
@@ -53,18 +61,21 @@ public class StudentAttendanceController implements Initializable
     private Label studentAttendancePercentage;
     @FXML
     private JFXToggleButton attendanceButton;
-    
+
     /**
      * Initializes the controller class.
      */
     @Override
-    public void initialize(URL url, ResourceBundle rb)
-    {
+    public void initialize(URL url, ResourceBundle rb) {
+        try {
+            checker();
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(StudentAttendanceController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @FXML
-    private void handleOverview(ActionEvent event) throws IOException
-    {
+    private void handleOverview(ActionEvent event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/attendance/automation/gui/view/StudentAttendanceOverview.fxml"));
 
         Parent root = (Parent) fxmlLoader.load();
@@ -72,20 +83,16 @@ public class StudentAttendanceController implements Initializable
         Stage stage = new Stage();
         stage.initModality(Modality.WINDOW_MODAL);
         stage.initStyle(StageStyle.TRANSPARENT);
-        root.setOnMousePressed(new EventHandler<MouseEvent>()
-        {
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
             @Override
-            public void handle(MouseEvent event)
-            {
+            public void handle(MouseEvent event) {
                 xOffset = event.getSceneX();
                 yOffset = event.getSceneY();
             }
         });
-        root.setOnMouseDragged(new EventHandler<MouseEvent>()
-        {
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
             @Override
-            public void handle(MouseEvent event)
-            {
+            public void handle(MouseEvent event) {
                 stage.setX(event.getScreenX() - xOffset);
                 stage.setY(event.getScreenY() - yOffset);
             }
@@ -109,23 +116,54 @@ public class StudentAttendanceController implements Initializable
     }
 
     @FXML
-    private void submitAttendance(ActionEvent event) throws SQLException
-    {
-        Date date = Calendar.getInstance().getTime();
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-        String strDate = dateFormat.format(date);
-        System.out.println("Converted String: " + strDate);
-        
-        int id = student.getId();
-        this.studentModel.updateAttendance(strDate, id);
-        
-        
-        
-        
-        
-//        progressBar.setProgress(0.92);
-//        studentAttendancePercentage.setText(92 + "%" + "");
-//        attendanceButton.setDisable(true);
+    private void submitAttendance(ActionEvent event) throws SQLException {
+        try {
+            if (checker() == true) {
+                Date date = Calendar.getInstance().getTime();
+                DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+                String strDate = dateFormat.format(date);
+                System.out.println("Converted String: " + strDate);
+
+                int id = student.getId();
+                this.studentModel.updateAttendance(strDate, id);
+            }
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(StudentAttendanceController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            if (checker() == false) {
+
+                attendanceButton.setSelected(false);
+                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                stage.setIconified(true);
+
+                Alert alert = new Alert(AlertType.INFORMATION);
+                alert.setTitle("Cannot submit attendance");
+                alert.setHeaderText(null);
+                alert.setContentText("Current location does not match with the school");
+
+                alert.showAndWait();
+            }
+
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(StudentAttendanceController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private boolean checker() throws UnknownHostException {
+        IpAddress = InetAddress.getLocalHost().getHostAddress();
+        System.out.println(IpAddress);
+
+        String[] adr = IpAddress.split("\\.");
+        for (int i = 0; i < adr.length - 1; i++) {
+            if (adr[0].equals("10") && adr[1].equals("176") && adr[2].equals("161")) {
+                System.out.println("Location matches the school");
+                return true;
+            }
+        }
+//                InetAddress addr = adresse.();
+        System.out.println("Location does not match the school");
+        return false;
     }
 
 }
