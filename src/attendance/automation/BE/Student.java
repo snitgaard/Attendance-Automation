26cc80deed4ccaeeb79d9b61d77938a63bbd004d
FimFi <55874743+FimFi@users.noreply.github.com>
2020-03-23 09:54:09 +0100

    Shit

--- src/attendance/automation/BE/Student.java
+++ src/attendance/automation/BE/Student.java
@@ -17,16 +17,19 @@ public class Student extends Person
     private String date;
     private int courseAttended;
     private String studentPassword;
+    private String studentEducation
     
-    public Student(int id, String name, String email, String course, double attendance, String date, int courseAttended, String studentPassword)
+    public Student(int id, String name, String email, String course, double attendance, String date, int courseAttended, String studentPassword, String studentEducation)
     {
         super(id, name, email, course);
         this.attendance = attendance;
         this.date = date;
         this.courseAttended = courseAttended;
         this.studentPassword = studentPassword;
+        this.studentEducation = studentEducation;
     }
 
+    
     public String getStudentPassword() {
         return studentPassword;
     }
